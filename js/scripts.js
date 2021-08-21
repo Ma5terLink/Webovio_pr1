2402222860b82b708ad5ee6403281e49e641bf59
Ma5terLink <devalon@yandex.ru>
2021-08-21 20:59:52 +0500

    07 - Добавлена последняя секция, подвал. Верстка закончена.The layout is completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-// Плавный скролл и класс pageup
 $(window).scroll(function () {
-    /* console.log($(this).scrollTop()) */
     if ($(this).scrollTop() > 700) {
         $('.pageup').fadeIn();
     } else {
@@ -8,51 +6,24 @@ $(window).scroll(function () {
     }
 });
 
-$("a[href^='#up']").click(function () { // При клике на ссылку, в которой значение атрибута href начинается с #up:
-    var _href = $(this).attr("href");  // создаём переменную _href и записываем в неё значение аттрибута href из кликнутой ссылки
+$("a[href^='#up']").click(function () { 
+    var _href = $(this).attr("href");  
     $("html, body").animate({scrollTop: '0'});
-    /* КАК РАБОТАЕТ ФУНКЦИЯ НИЖЕ - ПОКА НЕ ПОНЯЛ..
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"}); */
     return false;
 });
 
-// Переключение классов по нажатию куда-либо
-/* window.addEventListener('DOMContentLoaded', () => {
-const menuSalonsBtn = document.querySelector('.menu__linkbar-btn'),
-        salonsLinks = document.querySelector('.ourSalonsLinks'),
-        salonsItem = document.querySelectorAll('.ourSalonsLinks__item'),
-        salonsOverlay = document.querySelector('.ourSalonsLinks__overlay');
-    
-    salonsItem.forEach(item => {
-        item.addEventListener('click', () => {
-            menuSalonsBtn.classList.toggle('active')
-            salonsLinks.classList.toggle('active')
-        });
-    });
-    salonsOverlay.addEventListener('click', () => {
-        salonsLinks.classList.toggle('active')
-    });
-
-    menuSalonsBtn.addEventListener('click', () => {
-        salonsLinks.classList.toggle('active')
-    });
-}); */
-
-// СЛАЙДЕР SLICK
 $(document).ready(function() {
     $('.FS_slider').slick({
-        arrows: false, //стрелки.
-        dots: false, //"кнопки" (доты) выбора слайдов.
-        autoplay: true, //автопрокрутка.
-        autoPlaySpeed: 500, //скорость автопрокрутки.
-
+        arrows: false, 
+        dots: false, 
+        autoplay: true,
+        autoPlaySpeed: 500,
     });
     $('.FS_slider2').slick({
-        arrows: false, //стрелки.
-        dots: false, //"кнопки" (доты) выбора слайдов.
-        autoplay: true, //автопрокрутка.
-        autoPlaySpeed: 500, //скорость автопрокрутки.
-
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoPlaySpeed: 800,
     });
 });
 
